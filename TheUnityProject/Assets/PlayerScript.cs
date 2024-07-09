87d1a3732731bb04c238556847dda37b89951b1b
Daniel Kierkegaard Andersen <dax@daxode.dk>
2024-07-10 01:09:46 +0200

    day-1 b4 task-3

--- TheUnityProject/Assets/PlayerScript.cs
+++ TheUnityProject/Assets/PlayerScript.cs
@@ -4,13 +4,17 @@ public class PlayerScript : MonoBehaviour
 {
     public float speed = 10; // Speed of the player
 
+    Rigidbody m_RigidBody;
     void Start()
     {
-        
+        m_RigidBody = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
-        
+        Vector3 velocity = m_RigidBody.velocity;
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.z = Input.GetAxisRaw("Vertical") * speed;
+        m_RigidBody.velocity = velocity;
     }
 }
---
+++ TheUnityProject/Assets/ShootingScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShootingScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 0);
+    }
+}
